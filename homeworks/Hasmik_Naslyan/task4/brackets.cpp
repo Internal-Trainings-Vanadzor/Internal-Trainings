e20db0fc169acfb3179896c7b89fc36047eeb1df
hasmikNaslyan <hasmik_naslyan@mail.ru>
2015-06-13 01:00:08 +0400

    Improved task 4 to handle the following cases: 1. {}() 2. {.......a.......}

--- homeworks/Hasmik_Naslyan/task4/brackets.cpp
+++ homeworks/Hasmik_Naslyan/task4/brackets.cpp
@@ -1,32 +1,67 @@
 #include <iostream>
 #include <string>
+#include <stack>
+
 using namespace std;
 
-bool isBracketsCorrect(string brackets){
-    int bLength = brackets.length();
-    if (bLength%2 != 0){
-        return false;
-    }
-    for (int i=0; i<bLength/2; i++){
-        if ((brackets.at(i)=='{' && brackets.at(bLength-i-1)=='}')|| (brackets.at(i)=='[' && brackets.at(bLength-i-1)==']') || (brackets.at(i)=='(' && brackets.at(bLength-i-1)==')')){
-            return true;
+bool isBracketsCorrect(char openedBracket, char closedBracket){	   
+    if ((openedBracket=='{' && closedBracket =='}') || 
+            (openedBracket=='[' && closedBracket==']') ||
+            (openedBracket=='(' && closedBracket ==')')){
+        return true;
     }else{
         return false;
     }
+}
+
+bool isOpened(char openedBracket){
+    if (openedBracket=='{'||openedBracket=='['||openedBracket=='(') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isClosed(char closedBracket){
+    if (closedBracket=='}'||closedBracket==']'||closedBracket==')') {
+        return true;
+    } else {
+        return false;
     }
 }
 
 int main (){
-    string  brackets;    
-    cout<<"Input an expression containing brackets: ";
-    cin>>brackets;
-    if(isBracketsCorrect(brackets)){
-        std::cout<<"The order of the brackets is correct \n";
+    static string  expression; // I am not sure that having static 
+    static bool k = true;      // variable is preferable approach
+    static bool m = false;     // for these cases 	
+    int expLength; 
+    stack<char> brackets;
 
+    cout<<"Input an expression containing brackets: ";
+    cin>>expression;
+    expLength = expression.length();
+    for (int i=0; i<expLength; i++){
+        char bufer = expression.at(i);
+        if (isOpened(bufer)) {
+            brackets.push(bufer);
+            m = true;
+        }else{
+            if (isClosed(bufer)){
+                if(!brackets.empty()){
+                    if (isBracketsCorrect(brackets.top(), bufer)){				
+                        brackets.pop();
+                    }else{
+                        k = false;					
+                    }
+                }
+            }
+        }			
     }
-    else{
-        std::cout<<"The order of the brackets is incorrect \n";
-        
-    }
+
+    if(k&&brackets.empty()&&m) {
+        cout<<"The order of the brackets is correct \n";
+    }else{
+        cout<<"The order of the brackets is incorrect \n" ;        
+    } 
     return 0;
 }
