a6e195059b42c26b15051ee535029c2ffcb74150
hasmikNaslyan <hasmik_naslyan@mail.ru>
2015-06-13 23:57:45 +0400

    Author: Hasmik N. - Added validation for apostrophe.

--- homeworks/Hasmik_Naslyan/task4/brackets.cpp
+++ homeworks/Hasmik_Naslyan/task4/brackets.cpp
@@ -7,6 +7,8 @@ using namespace std;
 bool isBracketsCorrect(char openedBracket, char closedBracket){	   
     if ((openedBracket=='{' && closedBracket =='}') || 
             (openedBracket=='[' && closedBracket==']') ||
+            (openedBracket=='"' && closedBracket=='"') ||
+            (openedBracket=='\'' && closedBracket=='\'') ||
             (openedBracket=='(' && closedBracket ==')')){
         return true;
     }else{
@@ -30,38 +32,44 @@ bool isClosed(char closedBracket){
     }
 }
 
+bool isApostrophe(char isApostrophe){
+    if (isApostrophe=='"'||isApostrophe=='\'') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main (){
     static string  expression; // I am not sure that having static 
-    static bool k = true;      // variable is preferable approach
-    static bool m = false;     // for these cases 	
-    int expLength; 
+    static bool isAvarenny = false;      // variable is preferable approach for these cases 	
+    int expLength, i=0; 
     stack<char> brackets;
 
     cout<<"Input an expression containing brackets: ";
     cin>>expression;
-    expLength = expression.length();
-    for (int i=0; i<expLength; i++){
-        char bufer = expression.at(i);
-        if (isOpened(bufer)) {
-            brackets.push(bufer);
-            m = true;
-        }else{
-            if (isClosed(bufer)){
-                if(!brackets.empty()){
-                    if (isBracketsCorrect(brackets.top(), bufer)){				
-                        brackets.pop();
-                    }else{
-                        k = false;					
-                    }
-                }
-            }
-        }			
-    }
 
-    if(k&&brackets.empty()&&m) {
-        cout<<"The order of the brackets is correct \n";
+    expLength = expression.length();
+
+    while (!isAvarenny && i<expLength)  {
+        char bufer = expression.at(i);
+        
+	if (isOpened(bufer)) {brackets.push(bufer);}
+	if (isApostrophe(bufer)){
+	 if ( brackets.empty() || !isBracketsCorrect(brackets.top(), bufer)){
+		brackets.push(bufer);
+	 }else {brackets.pop();}
+	}
+	if (isClosed(bufer)){
+                if(!brackets.empty() && isBracketsCorrect(brackets.top(), bufer)){brackets.pop();}
+		else{isAvarenny=true;}
+	}
+	i++;
+    }
+    if(isAvarenny || !brackets.empty()) {
+        cout<<"The order of the brackets is incorrect \n";
     }else{
-        cout<<"The order of the brackets is incorrect \n" ;        
+       cout<<"The order of the brackets is correct \n" ;        
     } 
     return 0;
 }
