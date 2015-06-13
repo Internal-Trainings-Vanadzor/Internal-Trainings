10fc9bd2f2e6f78c28fe2feef8813222cd907a6b
Armenuhi Qocharyan <qarmenuhi@gmail.com>
2015-06-13 23:43:52 +0400

    Author: Armenuhi Qocharyan - Started correct implementation of StructureParser class accrding to Hamlet's feedback

--- homeworks/Armenuhi_Qocharyan/example_1/bracketsValidator/implementationStructureParser.h
+++ homeworks/Armenuhi_Qocharyan/example_1/bracketsValidator/implementationStructureParser.h
@@ -32,29 +32,37 @@ bool StructureParser::analyze(std::string  example) {
 	    std::map<std::string, char> mapOfSymbols = elements[i];
 	    for(std::map<std::string, char>::const_iterator 
 	        it = mapOfSymbols.begin(); it != mapOfSymbols.end(); ++it) {
-	        size_t openPosition =  example.find(mapOfSymbols["open"]);
-	        size_t closePosition =  example.find(mapOfSymbols["close"]);
 	        long openCount = std::count(example.begin(), example.end(), mapOfSymbols["open"]);
 	        long closeCount = std::count(example.begin(), example.end(), mapOfSymbols["close"]);
 	        if (openCount != closeCount) {
 	            this->isCorrect = false;
-	        }
+		    return this->isCorrect;
+		}
+	        size_t openPosition =  example.find_last_of(mapOfSymbols["open"]);
+		std::string ex2 = example.substr(openPosition +1, example.length()-openPosition);
+	        if (openPosition > example.length()) {
+			break;
+		}
+		std::string a= example.substr(openPosition, example.length()-openPosition);
+		size_t closePosition =  a.find(mapOfSymbols["close"]) + openPosition; 
 	        if (openPosition < closePosition ){
-		    example  =  example.erase(openPosition, 1);
-		    example = example.erase(closePosition - 1,  1);
-		    std::string s1 = example.substr(openPosition, closePosition - openPosition - 1);
-		    std::string s2 = example.substr(closePosition + 1, example.length() - closePosition -1);
+		    std::string s1 = example.substr(openPosition +1, closePosition - openPosition -1);
+		    std::string fierstPart =  example.substr(0,openPosition);
+		    std::string lastPart =  ex2.substr(ex2.find(mapOfSymbols["close"]) + 1,  ex2.length() - ex2.find(mapOfSymbols["close"]));
+		    std::string s2 = fierstPart + lastPart;
 		    StructureParser::analyze(s1);
 		    StructureParser::analyze(s2);
 	       } else  {
 	           if (openCount != 0){
 		       this->isCorrect = false;
+		       return this->isCorrect;
 		   }
 	       }
             }
         } 
     } catch (const std::out_of_range& oor) {
 	this->isCorrect = false;
+        return this->isCorrect;
     }
     return this->isCorrect;
 }
