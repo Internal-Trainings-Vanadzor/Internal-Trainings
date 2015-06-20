4c76c0dfcdea02f4e996fb0470e6b5cfd08d15b6
Armenuhi Qocharyan <qarmenuhi@gmail.com>
2015-06-20 11:28:20 +0400

    Author: Armenuhi Qocharyan - Removed unnecessary comments

--- homeworks/Armenuhi_Qocharyan/sharedPtr/my_shared_ptr.h
+++ homeworks/Armenuhi_Qocharyan/sharedPtr/my_shared_ptr.h
@@ -13,36 +13,3 @@ class sharedPtr {
         int* number;
 
 };
-/*
-sharedPtr::sharedPtr(int i) {
-    objectsCount = new int(0);
-    number = new int (i);
-}
-    
-sharedPtr::sharedPtr(const sharedPtr& object) {
-    number = object.number;
-    objectsCount = object.objectsCount;
-    (*objectsCount)++;
-}
-    
-void sharedPtr::operator=(const sharedPtr& b) {
-    // if my space is not used DELETE
-    number = b.number;
-    objectsCount = b.objectsCount;
-    (*objectsCount)++;
-}
-
-
-int sharedPtr::getCount() {
-    return *objectsCount;
-}
-
-sharedPtr::~sharedPtr() {
-    (*objectsCount) --;
-    if (*objectsCount == 0) {
-        delete number;
-        delete objectsCount;
-    }
-}
-
-*/
